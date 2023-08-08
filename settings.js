ce234fd40bddc76035c751fc52f70c73da307bf9
SkellenZ <141633572+SkellenZ@users.noreply.github.com>
2023-08-08 13:00:46 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -75,14 +75,14 @@ module.exports = {
     /** To password protect the Node-RED editor and admin API, the following
      * property can be used. See http://nodered.org/docs/security.html for details.
      */
-    //adminAuth: {
-    //    type: "credentials",
-    //    users: [{
-    //        username: "admin",
-    //        password: "$2a$08$zZWtXTja0fB1pzD4sHCMyOCMYz2Z6dNbM6tl8sJogENOMcxWV9DN.",
-    //        permissions: "*"
-    //    }]
-    //},
+    adminAuth: {
+        type: "credentials",
+        users: [{
+            username: "admin",
+            password: "$2y$10$p5KhEaYty3m/GRX2GgQG6.KNDqp.XqrBbRYrlxhCwbKdYZoroS5HG",
+            permissions: "*"
+        }]
+    },
     adminAuth: {
         type: "credentials",
         users: function (username) {
